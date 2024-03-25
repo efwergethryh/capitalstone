cd32912827724fcab388fbcf3aaf88781b331ca9
Mostafa <bttt8888444@gmail.com>
2024-03-25 19:44:30 +0300

    asddd

--- Controllers/PagesController.js
+++ Controllers/PagesController.js
@@ -2,7 +2,9 @@
 const get_landing_page = (req,res)=>{
     res.render('landingpage');
 }
-
+const get_allpics = (req,res)=>{
+    res.render('Allpics');
+}
 module.exports = {
-    get_landing_page
+    get_landing_page,get_allpics
 }
--- Routes/viewsRoutes.js
+++ Routes/viewsRoutes.js
@@ -3,5 +3,5 @@ const pagesController = require('../Controllers/PagesController')
 const router = express.Router();
 
 router.get('/',pagesController.get_landing_page);
-
+router.get('/Products',pagesController.get_allpics);
 module.exports = router
